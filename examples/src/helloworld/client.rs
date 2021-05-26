faea2ad7de29bc1f28d39e5f6d3e0503ce909aa4
Robert Masen <robert.masen@smartthings.com>
2021-05-26 17:09:20 -0500

    with uds and tokio 0.2

--- examples/src/helloworld/client.rs
+++ examples/src/helloworld/client.rs
@@ -1,19 +1,39 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
-
+use std::os::unix::net::UnixStream as StdUnixStream;
+use std::os::unix::io::{FromRawFd};
 pub mod hello_world {
     tonic::include_proto!("helloworld");
 }
-
+use tokio::net::UnixStream;
+use tonic::transport::Endpoint;
+use futures::future::ok;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let arg = std::env::args().nth(1).unwrap();
+    let fd: i32 = arg.parse().unwrap();
+    let std_socket = unsafe { StdUnixStream::from_raw_fd(fd) };
+    let socket = UnixStream::from_std(std_socket).unwrap_or_else(|e| {
+        eprintln!("failed to create unix stream from fd ({}): {}", fd, e);
+        // exit with EX_UNAVAILABLE
+        std::process::exit(69);
+    });
+    let mut socket_once = Some(socket); // HACK: service_fn below needs to be FnMut
+    let channel = Endpoint::from_static("null://localhost:50051")
+        .connect_with_connector(tower::service_fn(move |_| -> futures::future::Ready<std::io::Result<_>> {
+            ok(socket_once.take().unwrap())
+        }))
+        .await
+        .unwrap();
+    let mut client = GreeterClient::with_interceptor(channel, move|req: tonic::Request<()>| {
+        Ok(req)
+    });
 
     let request = tonic::Request::new(HelloRequest {
         name: "Tonic".into(),
     });
-
-    let response = client.say_hello(request).await?;
+    let response = tokio::time::timeout(std::time::Duration::from_secs(2),
+    client.say_hello(request)).await?;
 
     println!("RESPONSE={:?}", response);
 
--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -1,7 +1,15 @@
+use std::convert::Infallible;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio::io::{AsyncWrite};
+use tokio::io::AsyncRead;
+use tonic::transport::server::Connected;
 use tonic::{transport::Server, Request, Response, Status};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
+use std::os::unix::net::UnixStream as StdUnixStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -17,25 +25,76 @@ impl Greeter for MyGreeter {
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request from {:?}", request.remote_addr());
-
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name),
-        };
-        Ok(Response::new(reply))
+        let mut ct = 0;
+        loop {
+            println!("tick {}", ct);
+            ct += 1;
+            tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
+        }
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let (keep, to_send) = StdUnixStream::pair().unwrap();
+    let raw = to_send.as_raw_fd();
+    let without_cloexec = nix::unistd::dup(raw).unwrap();
+    let _without_cloexec_stream = unsafe { StdUnixStream::from_raw_fd(without_cloexec) };
+    std::mem::drop(to_send);
+    let stream = tokio::net::UnixStream::from_std(
+        keep
+    ).unwrap();
+    // let addr = "[::1]:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
-    println!("GreeterServer listening on {}", addr);
-
+    println!("GreeterServer listening on {}", without_cloexec);
+    
     Server::builder()
         .add_service(GreeterServer::new(greeter))
-        .serve(addr)
+        .serve_with_incoming(tokio::stream::once(Ok::<Uds, Infallible>(Uds(stream))))
         .await?;
-
+    tokio::process::Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg("helloworld-client")
+        .arg(&format!("{}", without_cloexec))
+        .spawn()
+        .unwrap()
+        .wait_with_output().await.unwrap();
     Ok(())
+}
+/// Wrapper around tokio's UnixStream
+#[derive(Debug)]
+pub struct Uds(pub tokio::net::UnixStream);
+
+impl Connected for Uds {}
+
+impl AsyncRead for Uds {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<std::io::Result<usize>> {
+        let pinned_inner = Pin::new(&mut self.0);
+        pinned_inner.poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for Uds {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        let pinned_inner = Pin::new(&mut self.0);
+        pinned_inner.poll_write(cx, buf)
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Pin::new(&mut self.0).poll_flush(cx)
+    }
+
+    fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        Pin::new(&mut self.0).poll_shutdown(cx)
+    }
 }
